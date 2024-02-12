748b0d1d9cca24ccf8303adb0122c83acb5b432d
レイン <sk.reinsp5@gmail.com>
2024-02-12 12:03:18 +0900

    ラッパー要素追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-  <Header class="bg-primary-950 h-20 min-h-20 relative" />
-  <main class="grow">
-    <slot />
-  </main>
-  <Footer class="p-4" />
+  <div>
+    <Header class="bg-primary-950 h-20 min-h-20 relative" />
+    <main class="grow">
+      <slot />
+    </main>
+    <Footer class="p-4" />
+  </div>
 </template>
 
 <style scoped></style>
